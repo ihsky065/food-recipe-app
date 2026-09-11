0fd294ed3c26592959da170b90095a6bc95950b4
Izzatul Husna <izzatulhusnaii@yahoo.com>
2026-09-12 04:46:38 +0800

    feat: add ingredients list and add matching class to unordered list

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const optionIdInput = document.getElementById('id-option');
 // variables of items inside box container
 const foodIdInput = document.getElementById('food-id');
 const foodName = document.getElementById('food-name');
+const ingredientsList = document.getElementById('ingredients-list');

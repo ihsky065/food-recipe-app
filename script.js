0f2e53c510e29b8cd14c889d36c62e933b986a0e
Izzatul Husna <izzatulhusnaii@yahoo.com>
2026-09-12 04:56:06 +0800

    feat: add image block and add matching class to image attribute

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const optionIdInput = document.getElementById('id-option');
 // variables of items inside box container
 const foodIdInput = document.getElementById('food-id');
 const foodName = document.getElementById('food-name');
+const imageBlock = document.getElementById('image-block');
 const ingredientsList = document.getElementById('ingredients-list');
 const instructionsList = document.getElementById('instructions-list');

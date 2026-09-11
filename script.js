b4fa0f6f0d5837c200830311fba023bf8754073d
Izzatul Husna <izzatulhusnaii@yahoo.com>
2026-09-12 04:51:54 +0800

    feat: add instructions list and add matching class to ordered list

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const optionIdInput = document.getElementById('id-option');
 const foodIdInput = document.getElementById('food-id');
 const foodName = document.getElementById('food-name');
 const ingredientsList = document.getElementById('ingredients-list');
+const instructionsList = document.getElementById('instructions-list');

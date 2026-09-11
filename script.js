68a15c803021ad4b2a62689ca9bfdda9eeb8ce60
Izzatul Husna <izzatulhusnaii@yahoo.com>
2026-09-12 06:03:51 +0800

    feat: implement a function of fetch recipe data

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ const foodName = document.getElementById('food-name');
 const imageBlock = document.getElementById('image-block');
 const ingredientsList = document.getElementById('ingredients-list');
 const instructionsList = document.getElementById('instructions-list');
+
+async function fetchRecipesData () {
+    const response = await fetch('https://dummyjson.com/recipes');
+
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    } else {
+        throw new Error('Error fetching food recipes data');
+    }
+}

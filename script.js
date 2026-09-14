c927afc18b6b135ab5389d2ef24a4e19e7aef1a7
Izzatul Husna <izzatulhusnaii@yahoo.com>
2026-09-15 04:18:04 +0800

    chore: clean up code and remove all unnesccary code for display selected ID

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const optionIdInput = document.getElementById('id-option');
-
-// variables of items inside box container
-const foodIdInput = document.getElementById('food-id');
-const foodName = document.getElementById('food-name');
-const imageBlock = document.getElementById('image-block');
-const ingredientsList = document.getElementById('ingredients-list');
-const instructionsList = document.getElementById('instructions-list');
-
-// const recipeBoxContainer = document.getElementsByClassName('box-container');
 const bodyContainer = document.getElementById('body-container');
 
 async function fetchRecipesData () {
@@ -21,52 +11,24 @@ async function fetchRecipesData () {
     }
 }
 
-function displayRecipeData () {
+ let recipeData;
+
+async function displayRecipeData (recipeData) {
     fetchRecipesData ()
     .then((recipeData) => {
-        bodyContainer.innerHTML = formatRecipeData (recipeData);
+       bodyContainer.innerHTML = formatRecipeData (recipeData);
     })
     .catch((error) => {
-        console.log('Error', error);
+       console.log('Error', error);
     })
 }
 
 displayRecipeData();
 
+let formatData = [];
+
 function formatRecipeData (recipeData) {
     const recipes = recipeData.recipes;
-
-    // for (const recipe of recipes) {
-    //     const divContainer = document.createElement('div');
-    //     divContainer.innerHTML =`
-    //     <div class="box-container">
-    //         <div class="left-section">
-    //             <h4 id="food-id">Food ID: <b>${recipe.id}</b></h4>
-    //             <p>Name:</p>
-    //             <p id="food-name"><b>${recipe.name}</b></p>
-    //             <img src="${recipe.image}" alt="food example" width="160px">
-    //         </div>
-    //         <div class="right-section">
-    //             <div class="text-content-first">
-    //             <p class="content-subtitle"><b>Ingredients:</b></p>
-    //             <ul id="ingredients-list">
-    //                 ${recipe.ingredients}
-    //             </ul>
-    //             </div>
-    //             <div class="text-content-second">
-    //             <p class="content-subtitle"><b>Instructions:</b></p>
-    //             <ol id="instructions-list">
-    //                 ${recipe.instructions}
-    //             </ol>
-    //         </div>
-    //         </div>
-    //     </div> 
-    //     `;
-    //     bodyContainer.appendChild(divContainer);
-    // }
-    // return bodyContainer.innerHTML;  
-
-    let formatData = [];
 
     for (const recipe of recipes) {
         formatData.push (`
@@ -91,8 +53,24 @@ function formatRecipeData (recipeData) {
                 </ol>
             </div>
             </div>
-        </div> 
+        </div> /-/
         `);
     }
-    return formatData.join('');  
+    return formatData.join('').split('/-/');
+}
+
+function displaySelectedID(recipeData) {  
+    const optionIdInput = document.getElementById('id-option');
+
+    if (optionIdInput.value == 'first-option') {
+        bodyContainer.innerHTML = `${formatData.slice(0, 6)}`;
+    } else if (optionIdInput.value == 'second-option') {
+        bodyContainer.innerHTML = `${formatData.slice(6, 12)}`;
+    } else if (optionIdInput.value == 'third-option') {
+        bodyContainer.innerHTML = `${formatData.slice(12, 18)}`;
+    } else if (optionIdInput.value == 'fourth-option') {
+        bodyContainer.innerHTML = `${formatData.slice(18, 24)}`;
+    } else if (optionIdInput.value == 'firth-option') {
+        bodyContainer.innerHTML = `${formatData.slice(24, 30)}`;
+    }
 }
